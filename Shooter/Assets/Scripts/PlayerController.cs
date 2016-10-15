24fcdd60ca6309ab5e83183f09e5450642be18cf
Chance Wen <sychancewen@gmail.com>
2016-10-14 21:03:06 -0700

    Basic game layout. Player and background added. Player movement functionality added.

---
+++ Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb;
+	public float speed;
+	public Boundary boundary;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, boundary.zMin, boundary.zMax)
+		);
+	}
+
+
+}
